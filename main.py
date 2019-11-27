1f7ca4dbc95232f823e0d63a6dfe3118fe81461c
MMaxM33 <39847299+MaxDiscord@users.noreply.github.com>
2019-11-26 19:20:34 -0800

    misc fixes and additions
    fixed a bunch of dumb errors and added the gather wood command

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 wood = 0
 food = 0
 population = 102
-water
+water = 0
 x = 0
-y = -7
+y = 0
+locx = 10
+locy = 10
 maxx = 20
 maxy = 20
 naturemap = [["Forest" for x in range(maxx)] for y in range(maxy)]
-naturemap[0][-7] = "Plains"
+naturemap[locx][locy] = "Plains"
 colonymap = [["None" for x in range(maxx)] for y in range(maxy)]
-colonymap[0][-7] = "Town Hall"
-print ("You have landed on a Plains tile. It looks like there is forest all around you")
-print ("All 102 people that survived the journey voted and they voted to stay and make the colony here.")
+colonymap[locx][locy] = "Town Hall"
+print ("You have landed on a",naturemap[locx][locy],"tile. It looks like there is forest all around you")
+print ("All",population," people that survived the journey voted and they voted to stay and make the colony here.")
 while True:
     mainInput = input ("> ")
-    if mainInput = ("w"):
+    if mainInput == ("w"):
         x = x - 1
     elif mainInput == ("e"):
         x = x + 1
     elif mainInput == ("n"):
-        y = y + 1
-    elif mainInput == ("s"):
         y = y - 1
-	elif mainInput == ("gather wood")
+    elif mainInput == ("s"):
+        y = y + 1
+    elif mainInput == ("gather wood"):
+        naturemap[x][y] = "Plains"
+        wood = wood + 5
