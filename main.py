38e8fe659e46f64470722f09ce85b97152fd9e1d
MMaxM33 <39847299+MaxDiscord@users.noreply.github.com>
2019-11-27 17:20:30 -0800

    added assorted features

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+import random
+import time
 wood = 0
 food = 0
 population = 10
@@ -8,6 +11,8 @@ maxx = 20
 maxy = 20
 popmax = 0
 turn = 1
+birthturn = 1
+starvingturn = 0
 townhall = False
 naturemap = [["Forest" for locx in range(maxx)] for y in range(maxy)]
 naturemap[locx][locy] = "Plains"
@@ -52,11 +57,28 @@ while True:
                 print ("You cannot build a farm here")
             else:
                 naturemap[locx][locy] == "Farm"
-                foodperturn = foodperturn + 10
+                if food < population:
+                    print ("People are starving")
+                    print ("You have 3 turns to fix it")
+                    starvingturn = starvingturn + 1
+                    if starvingturn == 3:
+                            print("You loose")
+                            sys.exit()
+        foodperturn = foodperturn + 10
+        food = food - population
+        turn = turn + 1
+        birthturn = birthturn + 1
+        if birthturn == 10:
+            population = population + 1
+            birthturn = 1
+        deadcrop = random.randint(1,30)
+        if deadcrop == 5:
+            food = food - foodperturn
+            print ("Your crops died")
+			
+
+				
+		
                 
                 
-                       
-            
-                       
-            
-    
+                
